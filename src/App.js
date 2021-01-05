ec3075d324b98f546d16e70e4dad701d29b8c63b
John Cross <john.william.cross@gmail.com>
2021-01-05 11:27:50 -0800

    install bootstrap, react, begin writing jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/master.scss"; // applies global scss styles
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-4">
+          <h1 className="text-center">John Cross</h1>
+          <h5 className="text-center">Full stack developer</h5>
+        </div>
+      </div>
     </div>
   );
 }
